0c201a078de3b4bec454b697faa2ca0f1ebb22c1
Ruvik <Kerinaguilera@gmail.com>
2025-02-12 14:18:46 +0100

    continuamos con el ejercicio en 3 en Raya

---
+++ Proyecto2Trimestre/Pruebas.java
@@ -0,0 +1,9 @@
+package Proyecto2Trimestre;
+
+public class Pruebas {
+    public static void main(String[] args) {
+        TresEnRaya juego = new TresEnRaya();
+        juego.DibujaTablero();
+
+    }
+}
--- Proyecto2Trimestre/TresEnRaya.java
+++ Proyecto2Trimestre/TresEnRaya.java
@@ -1,50 +1,62 @@
-//package Proyecto2Trimestre;
-//
-//public class TresEnRaya {
-//    private int tablero[][]= new int[3][3];
-//    private int pos = 0;
-//    public  TresEnRaya(){
-//            iniciar();
-//    }
-//    public void mueveJugador1(int pos){
-//
-//    }
-//    public void  mueveJugador2(int pos){
-//
-//    }
-//    public boolean movimientoValido(int pos){//averiguar si quedan movimiento validos....
-//        if (pos >= 1 && pos <= 9) {
-//            if (tablero[(pos - 1)/3][(pos-1)%3] == ' ') {
-//                return true;
-//            }
-//            return false;
-//        }
-//        return false;
-//    }
-//    public boolean quedanMovimientos(){
-//
-//    }
-//    public boolean ganaJugador1(){
-//
-//    }
-//    public boolean ganaJugador2(){
-//
-//    }
-//    public void DibujaTablero(){//mostramos el tablero
-//        System.out.println("-------------");
-//        System.out.println("| " + tablero[0][0] + " | " + tablero[0][1] + " | " + tablero[0][2] + " |");
-//        System.out.println("-------------");
-//        System.out.println("| " + tablero[1][0] + " | " + tablero[1][1] + " | " + tablero[1][2] + " |");
-//        System.out.println("-------------");
-//        System.out.println("| " + tablero[2][0] + " | " + tablero[2][1] + " | " + tablero[2][2] + " |");
-//        System.out.println("-------------");
-//    }
-//    //inicalizar el tablero
-//    public void iniciar() {
-//        int cont = 0;
-//        for (char i = '1'; i <= '9'; i++) {
-//            tablero[cont][cont] = i;
-//            cont++;
-//        }
-//
-//}
+package Proyecto2Trimestre;
+
+public class TresEnRaya {
+    private int tablero[][] = new int[3][3];
+    private int pos = 0;
+
+    public TresEnRaya() {
+        iniciar();
+    }
+
+    public void mueveJugador1(int pos) {
+
+    }
+
+    public void mueveJugador2(int pos) {
+
+    }
+
+    public boolean movimientoValido(int pos) {//averiguar si quedan movimiento válido...
+        if (pos >= 1 && pos <= 9) {
+            if (tablero[(pos - 1) / 3][(pos - 1) % 3] == ' ') {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    public boolean quedanMovimientos() {
+
+        return false;
+    }
+
+    public boolean ganaJugador1() {
+        return false;
+    }
+
+    public boolean ganaJugador2() {
+        return false;
+    }
+
+    public void DibujaTablero() {//mostramos el tablero
+        System.out.println("-----------------");
+        System.out.println("| " + tablero[0][0] + " | " + tablero[0][1] + " | " + tablero[0][2] + " |");
+        System.out.println("-----------------");
+        System.out.println("| " + tablero[1][0] + " | " + tablero[1][1] + " | " + tablero[1][2] + " |");
+        System.out.println("-----------------");
+        System.out.println("| " + tablero[2][0] + " | " + tablero[2][1] + " | " + tablero[2][2] + " |");
+        System.out.println("-----------------");
+    }
+
+    //inicalizar el tablero
+    public void iniciar() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                tablero[i][j] = 0;
+            }
+        }
+
+    }
+
+}
