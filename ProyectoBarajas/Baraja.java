e19dee74626b8127ebe559fc3d40b77829866ec4
Ruvik <Kerinaguilera@gmail.com>
2025-03-20 00:09:23 +0100

    Avazando Baraja

--- ProyectoBarajas/Baraja.java
+++ ProyectoBarajas/Baraja.java
@@ -1,7 +1,34 @@
 package ProyectoBarajas;
+
+import java.util.Random;
+
 //creamos la clase baraja
 public class Baraja {
     //inicalizamos las variables de esta clase.
-    private Carta lista_carta;
+    private Carta lista_cartas;
 
+    //creamos un constructor vacio
+    public Baraja (){
+
+    }
+    // creamos otro constructor con un tipo de bajara
+    public Baraja (int tipobaraja){
+        lista_cartas = new Carta();
+        lista_cartas = new Carta();
+    }
+    // creamos otro constructor esta ves con un boolean
+    public Baraja (int tipobaraja, boolean barajar){
+
+    }
+
+    public Carta Barajar(){
+        Carta lista_cartas2;
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            if (lista_cartas != null){
+
+            }
+        }
+
+    }
 }
--- ProyectoBarajas/Carta.java
+++ ProyectoBarajas/Carta.java
@@ -22,20 +22,23 @@ public class Carta {
     public int getPalo() {
         return palo;
     }
-
+    //creamos un metodo que nos dara el no mombre de la carta que nos toca
     public String NombreNumero(){
         String[] carta = {"as","dos","tres","cuatro","cinco","seis","siete","Caballo","Rey","sota"};
         return carta[numero-1];
 
     }
+    //este metodo nos devuelve un nombre del palo que elijamos.
     public String NombrePalo(){
        String[] palo1 = {"oro","copa","espada","bastos"};
        return palo1[palo -1];
     }
+    //este metodo nos devuelve el nombre de la carta entera utilizando los otros dos metodos
     public String  NombreDeCarta(){
         return NombreNumero()+" de "+NombrePalo();
 
     }
+    //este metodo nos servira para mas adelante es para dar un valor al modo de juego tute
     public int ValorTute(){
         if(numero>=8){
             return numero-6;
@@ -48,6 +51,7 @@ public class Carta {
         }
         return 0;
     }
+    //este metodo nos sirvira para un futuro nos devuelve la puntuación del juego del Mus
     public  int ValorMus(){
         if (numero <=2){
             return 1;
@@ -60,11 +64,18 @@ public class Carta {
         }
         return 0;
     }
+    //este metodo nos servira un futuro para que nos devuelva la puntación en el juego de Siete y medio
+    public double ValorSieteYMedio(){
+        if(numero >= 8){
+            return .50;
+        }
+        return 0;
+    }
 
     @Override
     public String toString() {
         return "numero= " + numero +NombreNumero() +
                 ", palo= " + palo + NombrePalo()+ " La Carta es: " + NombreDeCarta() +"\n El Valor del Tute es: "
-                +ValorTute()+"\n el valor en el mus es: "+ValorMus();
+                +ValorTute()+"\n el valor en el mus es: "+ValorMus()+"\n el valor de Siete y  Medio es: "+ValorSieteYMedio();
     }
 }
