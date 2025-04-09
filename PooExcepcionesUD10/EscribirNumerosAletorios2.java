d33267063946ae8fca50b6ca3978f06866f6ef09
Ruvik <Kerinaguilera@gmail.com>
2025-04-10 00:38:21 +0200

    acaba en clases

---
+++ PooExcepcionesUD10/EscribirNumerosAletorios2.java
@@ -0,0 +1,23 @@
+package PooExcepcionesUD10;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+public class EscribirNumerosAletorios2 {
+    public static void main(String[] args) {
+        FileWriter numAle;
+
+        try {
+            numAle = new FileWriter("NumerosAle.txt");
+            for (int i = 0; i < 10; i++) {
+                Random random = new Random();
+                String n = ""+random.nextInt(10);
+                numAle.write(n+"\n");
+            }
+            numAle.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
