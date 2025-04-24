2d60a10429a6a3a44670e75a4cecfa1a9999a84d
KerinR432 <kerinaguilera@gmail.com>
2025-04-24 14:18:46 +0200

    Tareas

---
+++ PooExcepcionesUD10/InvierteLineasFichero.java
@@ -0,0 +1,33 @@
+package PooExcepcionesUD10;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class InvierteLineasFichero {
+    public static void main(String[] args) {
+
+    }
+    public void invierteLinesFichero(String fichOrigen,fichDestino){
+        //leemos del origen y lo guardamos en el destino
+        String contenidoFichero = leerFicheroOrigen(fichOrigen);
+        //recorremos el String linea linea e invertimos la linea --> lo almacenamos en un arrayList
+        ArrayList<String>lineasInv=invierteLineaFich(contenidoFichero);
+        //recorremos el arrayList y lo gardamos en el fichero.
+        guardaFicheroDestino(contenidoFicheroInv);
+    }
+
+    private String leerFicheroOrigen(String fichOrigen){
+
+    }
+
+    private ArrayList<String> invierteLineaFich(String contenidoFichero){
+
+    }
+
+    private void guardaFicheroDestino(ArrayList<String >contenidoFicheroInv){
+
+    }
+}
